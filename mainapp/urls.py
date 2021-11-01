5cf34b2b07fc56de23c53fef51dc9dc0b6e8a10e
Akash Raj ST <akashraj49070@gmail.com>
2021-11-01 15:34:50 +0530

    name //fixed

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path('register', views.register),
     path('home',views.home,name="home"),
     path('profile/<int:user_id>',views.profile,name="profile"),
-    path('places/<int:p_id>/',views.place,name="places/<int:p_id>/"),
+    path('places/<int:p_id>/',views.place,name="places"),
     path('places/<int:p_id>/add_review',views.add_review),
     path('logout',views.logout,name="logout"),
 ]
