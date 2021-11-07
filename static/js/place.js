56d135c5eff9adfe87e109214e4410ac948ca7b2
Akash Raj ST <akashraj49070@gmail.com>
2021-11-07 19:26:26 +0530

    bgcolor //fixed

--- static/js/place.js
+++ static/js/place.js
@@ -9,7 +9,7 @@ function rev_spot(e,n){
     if(n==2){
         console.log("clicked spot");
         console.log("sub_place: ",sub_place);
-        spot.style.backgroundColor = "var(-light_pink)";
+        spot.style.backgroundColor = "var(--light_pink)";
         spot.style.textDecoration = "underline";
 
         rev.style.backgroundColor = "white";
@@ -25,7 +25,7 @@ function rev_spot(e,n){
 
     else{
         console.log("clicked Review");
-        rev.style.backgroundColor = "var(-light_pink)";
+        rev.style.backgroundColor = "var(--light_pink)";
         rev.style.textDecoration = "underline";
 
         spot.style.backgroundColor = "white";
